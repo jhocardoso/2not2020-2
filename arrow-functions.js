fb69b2df2b7b0652f7adb0958507455009b333f2
jhocardoso <jhonatancardoso17@hotmail.com>
2020-08-28 23:28:07 +0000

    inicio da atividade

--- arrow-functions.js
+++ arrow-functions.js
@@ -32,3 +32,45 @@ console.log(aleatorio())
 // Arrow function com 0 argumento
 // - Os parênteses vazios marcam o lugar do argumento (obrigatório)
 let aleatorio2 = () => Math.floor(Math.random() * 1000)
+
+// Situação alternativa 2: função com 1+ argumetno
+
+// Função tradicional com 1+ argumentos
+let imc = function(peso, altura) {
+    return peso / (altura ** 2)
+}
+
+// Arrow function com 1+ argumento
+// Os parâmetros dos argumetnos são obrigatórios quando nº arguemtnos > 1
+let imc2 = (peso, altura) => peso / (altura ** 2)
+
+console.log (imc(87, 1.78))
+console.log (imc2(87, 1.78))
+
+// CONCLUSÃO: os parâmetros só podem ser omitidos da lista de argumetnos 
+// de uma arrow function quando há UM e APENAS UM argumento.
+
+// Situação alternativa 3: funções com mais de uma linha no corpo
+
+// Função tradicional
+// 5! = 5 * 4 * 3 * 1 (120)
+let fatorial = function(n) {
+    let res = 1
+    for(let i = n; i > 1; i--) {
+        res *= i
+    }
+    return res
+}
+
+// Arrow function com corpo maior que uma linha
+// - Não há como fazer otimizações quanto ao corpo da função
+let fatorial2 = n => {
+    let res = 1
+    for(let i = n; i > 1; i--) {
+        res *= i
+    }
+    return res
+}
+
+console.log(fatorial(5))
+console.log(`O resultado é ${fatorial2(5)}.`)
