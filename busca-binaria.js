c9eb3c966e04f092272c50110f848416a44a2e46
jhocardoso <jhonatancardoso17@hotmail.com>
2020-09-11 20:20:43 +0000

    (04/09) Busca binária

--- busca-binaria.js
+++ busca-binaria.js
@@ -1,20 +1,45 @@
 // Pré-requisito para a busca binária: o conjunto de dados
 // PRECISA estar ordanado pelo critério de busca
+let comp = 0
 function buscaBinaria(lista, valorBusca) {
     let inicio = 0
     let fim = lista.length - 1
-    // Math.floor(): retira as casas decimais de um número. 
-    // Math.ceil() sempre arredonda pra cima    
-    // Math.round() faz a regra da matemática. Até 4 pra baixo e acima de 5 pra cima.
-    let meio = Math.floor((fim + inicio) / 2)
 
-    // Verifica se o valor na posição média é o valor de busca
-    if (valorBusca === lista[meio]) return meio
-    else if (valorBusca < lista[meio]) {
-        fim = meio - 1
+    while ( fim >= inicio) {
+        // Math.floor(): retira as casas decimais de um número. 
+        // Math.ceil() sempre arredonda pra cima    
+        // Math.round() faz a regra da matemática. Até 4 pra baixo e acima de 5 pra cima.
+        let meio = Math.floor((fim + inicio) / 2)
+
+        // Verifica se o valor na posição média é o valor de busca
+        if (valorBusca === lista[meio]) {
+            comp++
+            return meio
+        }
+        else if (valorBusca < lista[meio]) {
+            comp += 2
+            fim = meio - 1
+        }
+        else { //valorBusca > lista[meio]
+            comp += 2
+            inicio = meio + 1
+        }
     }
-    else { //valorBusca > lista[meio]
-        inicio = meio + 1
-    }
+    return -1       // Valor não encontrado
 
 }
+
+let vetorNomes = require('./dados/vetor-nomes')
+
+console.time('FAUSTO')
+console.log(buscaBinaria(vetorNomes, 'FAUSTO'))
+console.timeEnd('FAUSTO')
+console.log('Comparações: ', comp)
+
+console.log('---------------------------')
+
+comp = 0
+console.time('JHONATAN')
+console.log(buscaBinaria(vetorNomes, 'JHONATAN'))
+console.timeEnd('JHONATAN')
+console.log('Comparações: ', comp)
