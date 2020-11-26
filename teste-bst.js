6d5cab301cec04afb842fbf8e5b59ccdf79fa80f
jhocardoso <jhonatancardoso17@hotmail.com>
2020-11-26 23:28:38 +0000

    (26/11)  Continuação de ABB

--- teste-bst.js
+++ teste-bst.js
@@ -14,3 +14,30 @@ console.log(arvore)
 
 arvore.insert(41)
 console.log(arvore)
+
+arvore.insert(6)
+arvore.insert(70)
+arvore.insert(27)
+arvore.insert(8)
+arvore.insert(64)
+console.log(arvore)
+
+//arvore.inOrderTraversal(x => console.log(x))
+
+let vet = []
+arvore.inOrderTraversal(d => vet.push(d))
+console.log('Em-ordem:', vet)
+
+vet = []
+arvore.preOrderTraversal(d => vet.push(d))
+console.log('Pré-ordem:', vet)
+
+vet = []
+arvore.postOrderTraversal(d => vet.push(d))
+console.log('Pós-ordem:', vet)
+
+// Menor valor da árvore
+console.log("Mínimo: ", arvore.min())
+
+// Maior valor da árvore
+console.log("Máximo: ", arvore.max())
